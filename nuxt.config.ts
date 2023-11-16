0d2235bd4ea152f76cc6d9f61452fd83145fd892
andy <andy.thang@vand.vn>
2023-11-16 09:18:36 +0700

    feat: setup with mongodb

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -86,7 +86,7 @@ export default defineNuxtConfig({
     preference: "system", // default value of $colorMode.preference
     fallback: "light", // fallback value if not system preference found
     hid: "nuxt-color-mode-script",
-    globalName: "__NUXT_COLOR_MODE__",
+    globalName: "NUXT_COLOR_MODE",
     componentName: "ColorScheme",
     classPrefix: "",
     classSuffix: "-mode",
